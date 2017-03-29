66c5bde88e60fd49a7c93ddc451db1c3cf430170
jonnrauber <natanrauber97@gmail.com>
2017-03-28 22:32:10 -0300

    Configuração inicial do Bottle e inclusão do Materialize

---
+++ server/main.py
@@ -0,0 +1,12 @@
+from bottle import route, run, template, static_file, view
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='view/static')
+
+@route('/')
+@view('view/index')
+def start():
+    pass
+
+run(host='localhost', port=8080)
